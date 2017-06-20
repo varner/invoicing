d4830b5eb05e38e6c1a9d1935c5bee6bb1d4a0ce
Madeleine Varner <maddyvarner@gmail.com>
2017-06-20 16:33:48 -0400

    generates a satisfying pdf

--- run.py
+++ run.py
@@ -1,25 +1,59 @@
 import pdfkit
 from flask import Flask, request, render_template
-from datetime import date
+from datetime import datetime
+#from twilio.rest import Client
 app = Flask(__name__)
-
+#account_sid = "ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
+#auth_token = "your_auth_token"
+#client = Client(account_sid, auth_token)
 # INSTAGRAM FAX:
-#  SNAPCHAT FAX:
 #  FACEBOOK FAX:
+# --------------
+#  SNAPCHAT FAX:
 #   TWITTER FAX:
 
 @app.route("/", methods=['POST', 'GET'])
 def hello():
     if request.method == 'POST':
+        print request.form
+        rendered = ''
         posts = organizeItems(request.form)
+        print "\n\n\n"
         name  = request.form['name']
         email = request.form['email']
-        td    = date.today()
-        today = "%d/%d/%d" % (td.month, td.day, td.year)
-
-        render  = render_template('pdf.html', posts=posts[0], postcount=len(posts), name=request.form['name'])
-        filepdf = renderPDF(render)
-        return render_template('pdf.html', posts=posts[0], postcount=len(posts[0]), name=request.form['name'], today=today, email=email)
+        for post in posts:
+            #print post
+            if len(post['posts']) > 0:
+                td      = datetime.now()
+                today   = td.strftime("%m/%d/%Y")
+                social  = post['name']
+                render  = render_template('pdf.html',
+                    posts         = post['posts']                 ,
+                    postcount     = len(post['posts'])            ,
+                    invoice       = td.strftime("%Y%m%d%H%M%S")   ,
+                    company       = post['company']               ,
+                    name          = request.form['name']          ,
+                    email         = request.form['email']         ,
+                    tel           = request.form['phone']         ,
+                    social        = social                        ,
+                    today         = today                         ,
+                    address_line1 = request.form['address-line1'] ,
+                    address_line2 = request.form['address-line2'] ,
+                    country       = request.form['country']       ,
+                    city          = request.form['city']          ,
+                    region        = request.form['region']        ,
+                    postal_code   = request.form['postal-code']   ,  
+                    venmo         = request.form['venmo']         ,
+                    paypal        = request.form['paypal']        )
+                pdfname = td.strftime("%Y%m%d%H%M%S.pdf") # figure out how to format
+                filepdf = renderPDF(render, pdfname)
+                #fax = client.fax.v1.faxes.create(
+                #    from_="+15017250604", #our number
+                #    to=post['fax'], # whatever number
+                #    media_url="")
+        # FAX SHIT HERE
+        # WAIT FOR RESPONSE & THEN DELETE PDF (THIS IS A SAFE SPACE BITCH!!!!!)'''
+        return render
     return render_template('form.html')
 
 @app.route("/yerp/", methods=['POST', 'GET'])
@@ -29,7 +63,7 @@ def woof():
 if __name__ == "__main__":
     app.run()
 
-def renderPDF(render):
+def renderPDF(render, filename):
     options = {
             'print-media-type': '',
             'page-size': 'A4',
@@ -37,20 +71,20 @@ def renderPDF(render):
             'margin-left': '0.75in',
             'margin-right': '0.75in',
             'margin-top': '0.75in',
-            'dpi': 2000
+            'dpi': 1920
         }
-    filename = 'out.pdf'
     pdfkit.from_string(render, filename, options=options)
     return filename
 
 def organizeItems(form):
+    print "organizing....."
     sites = dict()
     for key in request.form.keys():
         if "_" in key:
             yerp = key.split("_")
             if yerp[0] not in sites.keys():
                 sites[yerp[0]] = dict()
-            sites[yerp[0]][yerp[1]] = request.form[key]
+            sites[yerp[0]][yerp[1]] = request.form[key] 
 
     facebook  = []
     twitter   = []
@@ -58,7 +92,6 @@ def organizeItems(form):
     instagram = []
 
     for key in sites.keys():
-        print key
         network = sites[key]['site']
         if   network == 'facebook' :
             facebook.append(sites[key])
@@ -68,8 +101,10 @@ def organizeItems(form):
             snapchat.append(sites[key])
         elif network == 'instagram':
             instagram.append(sites[key])
-    posts = [{'name':'Facebook', 'fax':'+10123456789', 'posts': facebook}, 
-             {'name':'Twitter',  'fax':'+10123456789', 'posts': twitter}, 
-             {'name':'Snapchat', 'fax':'+10123456789', 'posts': snapchat}, 
-             {'name':'Instagram','fax':'+10123456789', 'posts': instagram}]
+    posts = [{'name':'Facebook', 'company': 'Facebook Inc.', 'fax':'+16505434801', 'posts': facebook}, 
+             {'name':'Twitter',  'company': 'Twitter Inc.', 'fax':'+1xxxxxxxxxx', 'posts': twitter }, 
+             {'name':'Snapchat', 'company': 'Snap Inc.', 'fax':'+1xxxxxxxxxx', 'posts': snapchat}, 
+             {'name':'Instagram','company': 'Facebook Inc.', 'fax':'+16505434801', 'posts': instagram}]
+    print posts
+    print "....organized!"
     return posts
